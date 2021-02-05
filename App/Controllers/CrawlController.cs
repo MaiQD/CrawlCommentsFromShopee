fe048fc2a7ca6eabc11d140f2b4223bcce7102de
MaiQD <q.dat2710@gmail.com>
2021-02-05 13:28:27 +0700

    code refactoring

--- App/Controllers/CrawlController.cs
+++ App/Controllers/CrawlController.cs
@@ -1,4 +1,5 @@
 ﻿using App.CommonHelper;
+using App.Factories;
 using App.Models;
 using App.Services;
 using ClosedXML.Excel;
@@ -14,10 +15,12 @@ namespace App.Controllers
 	public class CrawlController : BaseController
 	{
 		private readonly IApiServices _apiServices;
+		private readonly IRatingFactory _ratingFactory;
 
-		public CrawlController(IApiServices apiServices)
+		public CrawlController(IApiServices apiServices,IRatingFactory ratingFactory)
 		{
 			_apiServices = apiServices;
+			_ratingFactory = ratingFactory;
 		}
 		public IActionResult CrawlOneProduct()
 		{
@@ -29,18 +32,9 @@ namespace App.Controllers
 			var list = new List<Rating>() ;
 			if (!string.IsNullOrEmpty(shopeeUrl))
 			{
-				var searhModel = shopeeUrl.GetItemAndShopIdFromUrl();
-				searhModel.type = commentStar;
-				while (true)
-				{
-					var res = await _apiServices.GetObjectApi<GetRatingResponseModel>(Common._urlGetRating, searhModel);
-					if (res?.data != null && res?.data?.ratings != null && res.data.ratings.Count > 0)
-						list.AddRange(res.data.ratings.Where(p => !string.IsNullOrEmpty(p.comment)));
-					else
-						break;
-					searhModel.offset += searhModel.limit;
-				}
-				
+				var searchModel = shopeeUrl.GetItemAndShopIdFromUrl();
+				searchModel.type = commentStar;
+				list = await _ratingFactory.PrepareRatingsForOneProductAsync(searchModel);
 			}
 			
 			return ShowCrawledData(list);
@@ -50,42 +44,21 @@ namespace App.Controllers
 			var list = new List<Rating>();
 			if (!string.IsNullOrEmpty(shopeeUrl))
 			{
-				var searhModel = shopeeUrl.GetItemAndShopIdFromUrl();
-				searhModel.type = commentStar;
-				while (true)
-				{
-					var res = await _apiServices.GetObjectApi<GetRatingResponseModel>(Common._urlGetRating, searhModel);
-					if (res?.data != null && res?.data?.ratings != null && res.data.ratings.Count > 0)
-						list.AddRange(res.data.ratings.Where(p => !string.IsNullOrEmpty(p.comment)));
-					else
-						break;
-					searhModel.offset += searhModel.limit;
-				}
-
+				var searchModel = shopeeUrl.GetItemAndShopIdFromUrl();
+				searchModel.type = commentStar;
+				list = await _ratingFactory.PrepareRatingsForOneProductAsync(searchModel);
 			}
-
+			return GetRatingsFile(list);
+		}
+		private IActionResult GetRatingsFile(List<Rating> list)
+		{
 			string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-
-			string fileName = DateTime.Now.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK")+".xlsx";
+			string fileName = DateTime.Now.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK") + ".xlsx";
 			try
 			{
 				using (var workbook = new XLWorkbook())
 				{
-					IXLWorksheet worksheet =
-					workbook.Worksheets.Add("Authors");
-					worksheet.Cell(1, 1).Value = "Id";
-					worksheet.Cell(1, 2).Value = "Rating Star";
-					worksheet.Cell(1, 3).Value = "Username";
-					worksheet.Cell(1, 4).Value = "Comment";
-					int index =  1;
-					foreach (var item in list)
-					{
-						worksheet.Cell(index + 1, 1).Value = index;
-						worksheet.Cell(index + 1, 2).Value = item.rating_star;
-						worksheet.Cell(index + 1, 3).Value = item.author_username;
-						worksheet.Cell(index + 1, 4).Value = item.comment;
-						index++;
-					}
+					SetUpWorkBook(workbook, list);
 					using (var stream = new MemoryStream())
 					{
 						workbook.SaveAs(stream);
@@ -98,8 +71,24 @@ namespace App.Controllers
 			{
 				return RedirectToAction("Error", "Home");
 			}
-
 		}
-
+		private void SetUpWorkBook(XLWorkbook workbook,List<Rating> ratings)
+		{
+			IXLWorksheet worksheet =
+					workbook.Worksheets.Add("Authors");
+			worksheet.Cell(1, 1).Value = "Id";
+			worksheet.Cell(1, 2).Value = "Rating Star";
+			worksheet.Cell(1, 3).Value = "Username";
+			worksheet.Cell(1, 4).Value = "Comment";
+			int index = 1;
+			foreach (var item in ratings)
+			{
+				worksheet.Cell(index + 1, 1).Value = index;
+				worksheet.Cell(index + 1, 2).Value = item.rating_star;
+				worksheet.Cell(index + 1, 3).Value = item.author_username;
+				worksheet.Cell(index + 1, 4).Value = item.comment;
+				index++;
+			}
+		}
 	}
 }
---
+++ App/Factories/RatingFactory.cs
@@ -0,0 +1,38 @@
+﻿using App.CommonHelper;
+using App.Models;
+using App.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Factories
+{
+	public interface IRatingFactory
+	{
+		Task<List<Rating>> PrepareRatingsForOneProductAsync(RatingSearchModel searchModel);
+	}
+	public class RatingFactory : IRatingFactory
+	{
+		private readonly IApiServices _apiServices;
+
+		public RatingFactory(IApiServices apiServices)
+		{
+			_apiServices = apiServices;
+		}
+		public virtual async Task<List<Rating>> PrepareRatingsForOneProductAsync(RatingSearchModel searchModel)
+		{
+			var list = new List<Rating>();
+			while (true)
+			{
+				var res = await _apiServices.GetObjectApi<GetRatingResponseModel>(Common._urlGetRating, searchModel);
+				if (res?.data != null && res?.data?.ratings != null && res.data.ratings.Count > 0)
+					list.AddRange(res.data.ratings.Where(p => !string.IsNullOrEmpty(p.comment)));
+				else
+					break;
+				searchModel.offset += searchModel.limit;
+			}
+			return list;
+		}
+	}
+}
